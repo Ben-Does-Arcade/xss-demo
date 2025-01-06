d94825dfe3151fd1d4a218542db30ef0ea030afa
Ben <bendoesarcade@gmail.com>
2025-01-06 14:00:46 -0600

    Handle nav link

--- attack1.js
+++ attack1.js
@@ -1,4 +1,19 @@
+// Inject XSS - FUN!
 const params = new URLSearchParams(window.location.search);
 const content = document.querySelector("#xss-content");
 
-content.innerHTML = "Hello, " + params.get("username") + "!";
+document.addEventListener("DOMContentLoaded", () => {
+    content.innerHTML = "Hello, " + params.get("username") + "!";  // innerHTML is the reason this works
+});
+
+// Handle toggling the navigation between "Back" and "Close"
+const navButton = document.querySelector("#nav-back");
+
+if (window.opener && window.opener !== window) {
+    navButton.href = "#";
+    navButton.innerHTML = "&cross; Close";
+
+    navButton.addEventListener("click", () => {
+        window.close();
+    });
+}
