a94dab91db55eb3127b893552ea8e9377342a1b6
Ben <bendoesarcade@gmail.com>
2025-01-06 16:19:31 -0600

    Add cookie logic, handle opening attacks

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const attack1Field = document.querySelector("#attack1");
 const attack1Run = document.querySelector("#attack1-run");
+const attack1Copy = document.querySelector("#attack1-copy");
 const attack1Clear = document.querySelector("#attack1-clear");
+const attack1Label = document.querySelector("#attack1-label");
+const attack1CookieInput = document.querySelector("#attack1-cookie");
+const attack1CookieValue = document.querySelector("#attack1-cookie-value");
+const attack1CookieSet = document.querySelector("#attack1-cookie-set");
+const attack1CookieReset = document.querySelector("#attack1-cookie-reset");
+
+// Add a cookie for testing
+document.cookie = "session=my_secret_session_token";
 
 // Handle automatically resizing the textarea for attack 1 demo
 attack1Field.style.height = "calc(" + attack1Field.scrollHeight + "px - 10px)"; // JS inline CSS calc! >:)
@@ -13,23 +22,63 @@ attack1Field.addEventListener("input", () => {
 
 // Navigate to endpoint on button press or enter keystroke
 function encodeAttack1() {
+    return "http://localhost:63342/xss-demo/attack1.html?username=" + encodeURI(attack1Field.value);
+}
+
+function openAttack1() {
     window.open(
-        "http://localhost:63342/xss-demo/attack1.html?username=" + encodeURI(attack1Field.value),
+        encodeAttack1(),
         null,
         "popup=true"
     );
 }
 
-attack1Run.addEventListener("click", encodeAttack1);
+attack1Run.addEventListener("click", openAttack1);  // Handle run button press
+attack1Field.addEventListener("keypress", (e) => {  // Handle enter keystroke
+    if (e.ctrlKey && e.code === "Enter") {
+        e.preventDefault();  // Prevent strange things from happening by the browser
+        openAttack1();
+    }
+});
 
-attack1Clear.addEventListener("click", (e) => {
+// Handle clear button press
+attack1Clear.addEventListener("click", () => {
     attack1Field.value = "";
     attack1Field.style.height = "auto";
     attack1Field.style.height = "calc(" + attack1Field.scrollHeight + "px - 10px)";
 });
 
-attack1Field.addEventListener("keypress", (e) => {
-    if (e.ctrlKey && e.code === "Enter") {
-        encodeAttack1();
+// Handle copy link button press
+attack1Copy.addEventListener("click", () => {
+    navigator.clipboard.writeText(encodeAttack1()).then(() => {
+        // Show that the text was copied successfully
+        const originalText = attack1Label.textContent;
+        attack1Label.style.color = "green";
+        attack1Label.innerHTML = "&check; Copied link to clipboard!";
+
+        // Reset after 1.5 seconds
+        setTimeout(() => {
+            attack1Label.style.color = "black";
+            attack1Label.textContent = originalText;
+        }, 1500);
+    });
+});
+
+// Handle cookie set button press
+function setCookie() {
+    document.cookie = "session=" + attack1CookieInput.value;
+    attack1CookieValue.textContent = attack1CookieInput.value;
+    attack1CookieInput.value = "";
+}
+
+attack1CookieSet.addEventListener("click", setCookie);    // Handle save button press
+attack1CookieInput.addEventListener("keypress", (e) => {  // Handle enter keystroke
+    if (e.code === "Enter") {
+        setCookie();
     }
+});
+
+// Handle cookie reset button press
+attack1CookieReset.addEventListener("keypress", () => {
+    document.cookie = "session=my_secret_session_token";
 });
